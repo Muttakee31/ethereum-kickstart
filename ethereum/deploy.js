e389564ebc195daada3eb377ec95bdbc31a98bf3
muttakee31 <saad@field.buzz>
2021-06-19 01:22:15 +0600

    moved js files to ethereum directory

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -1,6 +1,6 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
-const compiledFactory = require('./ethereum/build/CampaignFactory.json');
+const compiledFactory = require('./build/CampaignFactory.json');
 const provider = new HDWalletProvider(
     'man hair melody legal drastic few illegal phrase trick negative grace juice',
     'https://rinkeby.infura.io/v3/7b5ccc40b4254532b07f0a482e1a5d0f');
